6a7ea951dccad50627282b51b5fcf25ce0a5e8ec
ProCoder5534 <ragini104singh@gmail.com>
2023-07-27 17:54:03 +0530

    OKKKKK

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
             background-image: linear-gradient(to bottom left, #87cefa, #a020f0);
         }
     </style>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body class="bg-gradient-to-tl from-blue-300 to-purple-500 h-screen">
@@ -40,6 +41,60 @@
         <div class="inside">
 
         </div>
+    </div>
+    <!--the cards -->
+    <div class="flex gap-10 container" >
+    <div class="relative max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 transition-border hover:border-red-500">
+    <!-- The icon image -->
+    <div class="absolute top-2 right-2">
+      <img src="images/locked.png" alt="Icon" class="w-5 h-5">
+    </div>
+  
+    <a href="#">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Employee List</h5>
+    </a>
+    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">The Employee List of Abc Corporation (Confidential)</p>
+    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      View Database
+      <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+      </svg>
+    </a>
+  </div>
+  <div class="relative max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 transition-border hover:border-red-500">
+    <!-- The icon image -->
+    <div class="absolute top-2 right-2">
+      <img src="images/locked.png" alt="Icon" class="w-5 h-5">
+    </div>
+  
+    <a href="#">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Employee List</h5>
+    </a>
+    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">The Employee List of Abc Corporation (Confidential)</p>
+    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      View Database
+      <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+      </svg>
+    </a>
+  </div>
+  <div class="relative max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 transition-border hover:border-red-500">
+    <!-- The icon image -->
+    <div class="absolute top-2 right-2">
+      <img src="images/locked.png" alt="Icon" class="w-5 h-5">
+    </div>
+  
+    <a href="#">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Employee List</h5>
+    </a>
+    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">The Employee List of Abc Corporation (Confidential)</p>
+    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      View Database
+      <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+      </svg>
+    </a>
+  </div>
     </div>
     <script src="https://cdn.tailwindcss.com"></script>
 </body>
