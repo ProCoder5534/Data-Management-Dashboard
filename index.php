6e4289cdd36d2b237e27d4c32dc0812390c2a4eb
FIRST_NAME LAST_NAME <amaanhussain1210@gmail.com>
2023-07-27 17:31:34 +0530

    idk

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <html>
 
 <head>
-    
+
     <link rel="icon" type="image/x-icon" href="images\favicon.png">
+    <link rel="stylesheet" href="extra.css">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
@@ -13,16 +14,21 @@
 </head>
 
 <body>
-   <div class="nav bg-blue-700 h-16 rounded-b-3xl">
-     <nav>
-       <ul class="relative h-32 w-screen">
-        <li class="absolute top-0 left-12 "><img src="./images/favicon.png" alt="LOGO" class="h-16 w-16 rounded-full"></li>
-        <li class="absolute top-3 right-[200] "><button class="h-10 w-32 rounded-2xl bg-yellow-300 hover:rounded-full hover:border-4 border-blue-300 font-semibold"> Credits </button></li>
-        <li class="absolute right-12 top-3"><button class="h-10 w-32 rounded-2xl bg-yellow-300 hover:rounded-full hover:border-4 border-blue-300 font-semibold"> About </button></li>
-       </ul>
-     </nav>
-   </div>
-   <script src="https://cdn.tailwindcss.com"></script>
+    <div class="nav bg-blue-700 h-16 rounded-b-3xl">
+        <nav>
+            <ul class="relative h-32 w-screen">
+                <li class="absolute top-0 left-12 "><img src="./images/favicon.png" alt="LOGO"
+                        class="h-16 w-16 rounded-full"></li>
+                <li class="absolute top-3 right-[200] "><button
+                        class="h-10 w-32 rounded-2xl bg-yellow-300 hover:rounded-full hover:border-4 border-blue-300 font-semibold">
+                        Credits </button></li>
+                <li class="absolute right-12 top-3"><button
+                        class="h-10 w-32 rounded-2xl bg-yellow-300 hover:rounded-full hover:border-4 border-blue-300 font-semibold">
+                        About </button></li>
+            </ul>
+        </nav>
+    </div>
+    <script src="https://cdn.tailwindcss.com"></script>
 </body>
 
 </html>
