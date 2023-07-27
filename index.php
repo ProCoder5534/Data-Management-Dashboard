5f687f3e736a6234a406efd90bd3a5302bf2c272
BuildTools <unconfigured@null.spigotmc.org>
2023-07-27 16:43:40 +0530

    noting haha

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <html>
 
 <head>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
-        crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    
     <link rel="icon" type="image/x-icon" href="images\favicon.png">
 </head>
 
 <body>
-
+   <div class="nav bg-blue-700 h-16 rounded-b-3xl">
+     <nav>
+       <ul class="relative h-32 w-screen">
+        <li class="absolute top-0 left-12 "><img src="./images/favicon.png" alt="LOGO" class="h-16 w-16 rounded-full"></li>
+        <li class="absolute top-3 right-[200] "><button class="h-10 w-32 rounded-2xl bg-yellow-300 hover:rounded-full hover:border-4 border-blue-300 font-semibold"> Credits </button></li>
+        <li class="absolute right-12 top-3"><button class="h-10 w-32 rounded-2xl bg-yellow-300 hover:rounded-full hover:border-4 border-blue-300 font-semibold"> About </button></li>
+       </ul>
+     </nav>
+   </div>
+   <script src="https://cdn.tailwindcss.com"></script>
 </body>
 
 </html>
