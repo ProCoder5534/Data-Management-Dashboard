9e14e71b8e24d59b2bbbbb0619d3b70f96f88e59
BuildTools <unconfigured@null.spigotmc.org>
2023-07-30 17:24:44 +0530

    Added animations to search

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
 
     // Define the search function in the global scope
-    const search = () => {
+    const fadeIn = (element) => {
+        element.style.display = 'block'; // Show the card
+        element.style.opacity = 0; // Set initial opacity to 0
+    
+        let opacity = 0;
+        const duration = 500; // Adjust the duration as needed
+        const interval = 20; // Interval for the animation
+        const increment = interval / duration;
+    
+        const animate = () => {
+          opacity += increment;
+          element.style.opacity = opacity;
+    
+          if (opacity < 1) {
+            requestAnimationFrame(animate);
+          }
+        };
+    
+        requestAnimationFrame(animate);
+      };
+    
+      // Function to fade out a card
+      const fadeOut = (element) => {
+        let opacity = 1;
+        const duration = 500; // Adjust the duration as needed
+        const interval = 20; // Interval for the animation
+        const increment = interval / duration;
+    
+        const animate = () => {
+          opacity -= increment;
+          element.style.opacity = opacity;
+    
+          if (opacity > 0) {
+            requestAnimationFrame(animate);
+          } else {
+            element.style.display = 'none'; // Hide the card when the animation completes
+          }
+        };
+    
+        requestAnimationFrame(animate);
+      };
+    
+      const search = () => {
         const searchValue = document.querySelector(".searchInput").value.trim().toLowerCase();
         console.log("Search Value:", searchValue);
     
@@ -12,10 +54,10 @@
     
           if (headingText.includes(searchValue)) {
             console.log('FOUND IT');
-            card.classList.remove('hidden'); // Show the card if it matches the search
+            fadeIn(card); // Fade in the card if it matches the search
           } else {
             console.log('NOT FOUND');
-            card.classList.add('hidden'); // Hide the card if it doesn't match the search
+            fadeOut(card); // Fade out the card if it doesn't match the search
           }
         }
       };
